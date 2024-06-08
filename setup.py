6aaa72e7c84a579d67bc00279f701099f5783226
Chanchal Sahu <chanch1001>
2024-06-08 19:39:20 +0530

    added env details

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 # To download in local folder
 
 from setuptools import find_packages, setup
-
-
+#/ineuron_Seven_project/interview-question-creator
+#https://whimsical.com/JAhkxhoPHGXa7B1DdPN55
 setup(
     name= "Generative AI Project",
     version= "0.0.0",
     author="Chanchal",
-    author_email="entbabby73@gmail.com",
+    author_email="entbuppy73@gmail.com",
     packages=find_packages(),
     install_requries = []
 )
