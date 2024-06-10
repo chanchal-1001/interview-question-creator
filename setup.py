436ff43d358b09a5e900cd1fec6217270204a43a
Chanchal Sahu <chanch1001>
2024-06-10 12:10:14 +0530

    project ready

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@
 
 from setuptools import find_packages, setup
 #/ineuron_Seven_project/interview-question-creator
+#https://github.com/entbappy/Interview-Question-Creator/blob/main/src/helper.py
+
 #https://whimsical.com/JAhkxhoPHGXa7B1DdPN55
+
+#https://github.com/entbappy/Deploy-Streamlit-app-on-EC2-instance
+
+#https://askubuntu.com/questions/1465218/pip-error-on-ubuntu-externally-managed-environment-%C3%97-this-environment-is-extern
+
 setup(
     name= "Generative AI Project",
     version= "0.0.0",
