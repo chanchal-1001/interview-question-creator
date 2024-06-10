5248a61a4822e632cfcc905563d53d23edda8de1
Chanchal Sahu <chanch1001>
2024-06-10 12:47:59 +0530

    deleted output folder

--- setup.py
+++ setup.py
@@ -19,3 +19,20 @@ setup(
     packages=find_packages(),#looks for __init__ to setup in local sy
     install_requries = []
 )
+
+# sudo apt update
+# sudo apt-get update
+# sudo apt upgrade -y
+# sudo apt install git curl unzip tar make sudo vim wget -y
+# git clone "Your-repository"
+# touch .env to generate new file
+# sudo apt install python3-pip
+# sudo apt install python3-venv
+# python3 -m venv .venv
+# source .venv/bin/activate
+# pip3 install -r requirements.txt
+# #Temporary running
+# python3 -m streamlit run app.py
+# #Permanent running
+# nohup python3 -m streamlit run app.py
+# Note: Streamlit runs on this port: 8501
